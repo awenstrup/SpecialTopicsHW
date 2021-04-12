cefc5a4be7b251cfc75f4671a076214afdbf9c3a
Alex Wenstrup <awenstrup@olin.edu>
2021-04-12 16:00:05 -0400

    Finish branch and bound implementation

--- BAB/bab_starter.py
+++ BAB/bab_starter.py
@@ -28,8 +28,12 @@ class BBTreeNode():
         Bulids the initial Picos problem
         '''
         prob=pic.Problem()
-   
-        prob.add_list_of_constraints(self.constraints)    
+
+        if type(self.constraints) is not list:
+           c = [self.constraints[x] for x in self.constraints]
+        else:
+            c = self.constraints
+        prob.add_list_of_constraints(c)    
         
         prob.set_objective('max', self.objective)
         self.prob = prob
@@ -50,6 +54,7 @@ class BBTreeNode():
         '''
         n1 = deepcopy(self)
         n1.prob.add_constraint( branch_var <= math.floor(branch_var.value) ) # add in the new binary constraint
+        n1.constraints = n1.prob.constraints
 
         return n1
 
@@ -59,6 +64,8 @@ class BBTreeNode():
         '''
         n2 = deepcopy(self)
         n2.prob.add_constraint( branch_var >= math.ceil(branch_var.value) ) # add in the new binary constraint
+        n2.constraints = n2.prob.constraints
+
         return n2
 
 
@@ -74,13 +81,34 @@ class BBTreeNode():
 
         # these lines build up the initial problem and adds it to a heap
         root = self
-        res = root.buildProblem().solve(solver='cvxopt')
-        heap = [(res, next(counter), root)]
         bestres = -1e20 # a small arbitrary initial best objective value
         bestnode_vars = root.vars # initialize bestnode_vars to the root vars
 
-        #TODO: fill this part in
+        try:
+            res = root.buildProblem().solve(solver='cvxopt')
+            heap = [(res, next(counter), root)]
+        except pic.modeling.problem.SolutionFailure:
+            # Base case: no valid solution
+            return bestres, list(bestnode_vars)
 
-        
-        return bestres, bestnode_vars
+        # Base case: is integral
+        if self.is_integral():
+            return float(res.reportedValue), list(self.vars)
+
+        # Recursive case
+        children = []
+        for v in self.vars[:-1]:
+            if abs(round(v.value) - float(v.value)) > 1e-4: # v isn't an integer yet
+                children.append(self.branch_floor(v))
+                children.append(self.branch_ceil(v))
+
+        def get_val(x):
+            out = x.bbsolve()[0]
+            try:
+                return float(out.reportedValue)
+            except:
+                return out
+
+        opt = max(children, key = get_val)
+        return get_val(opt), list(opt.vars)
  
